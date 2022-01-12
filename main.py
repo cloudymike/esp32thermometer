303e06ee7a161503f8e2ec734c240911da72bac2
cloudymike <mikael@cloudyforecast.com>
2022-01-11 22:30:23 -0800

    Fancy but not curl friendly

--- main.py
+++ main.py
@@ -32,21 +32,39 @@ def displayLambda(tempDevice, oled, rtc, ip):
     bignumber.bigTemp(oled, temperature, unit)
     oled.show()
 
+###############################################
+# Web pages
+# Complete html (or not for raw text)
+# Called from "routes"
+###############################################
 def web_page():
   if LED.LED.value() == 1:
-    gpio_state="ON"
+    gpio_state="LED ON"
   else:
-    gpio_state="OFF"
+    gpio_state="LED OFF"
 
-  html = """<html><head> <title>ESP Web Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
-  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none;
-  border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #4286f4;}</style></head><body> <h1>ESP Web Server</h1>
-  <p>Blue LED: <strong>""" + gpio_state + """</strong></p><p><a href="/?led=on"><button class="button">ON</button></a></p>
-  <p><a href="/?led=off"><button class="button button2">OFF</button></a></p><p>Temperature: <strong>""" + str(temperature) + """</strong></p></body></html>"""
+  html = """<html><head> <title>Thermometer Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+  html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}
+  p{font-size: 1.5rem;}
+  .button{display: inline-block; background-color: #4286f4; border: none;border-radius: 4px;
+          color: white; padding: 16px 40px; text-decoration: none; font-size: 30px;
+          margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}
+  </style>
+  </head><body>
+  <h1>Thermometer Server</h1>
+  <p><strong>Temperature: """ + str(temperature) + """</strong></p>
+  <p><a href="/?led=toggle"><button class="button button">""" + gpio_state + """</button></a></p>
+  <p style="font-size: 10px;">Route for just temperature: <a href="/temperature">/temperature</a></p>
+  </body></html>"""
   return html
 
+# Just the temperature number. Think curl
+def temperature_page():
+    return(str(temperature))
 
 ###############################################
 # Setup WLAN
@@ -57,7 +75,7 @@ s = socket.socket()
 # Allow reuse of socket address on softreboot
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
-s.listen(1)
+s.listen(5)
 ifconfig = network.WLAN(network.STA_IF).ifconfig()
 ip = ifconfig[0]
 
@@ -101,23 +119,23 @@ displaytimer.init(period=1000,
 
 print('Starting while loop')
 while(1):
-  #LED.LED.value(abs(LED.LED.value()-1))
-  conn, addr = s.accept()
-  print('Got a connection from %s' % str(addr))
-  request = conn.recv(1024)
-  request = str(request)
-  print('Content = %s' % request)
-  led_on = request.find('/?led=on')
-  led_off = request.find('/?led=off')
-  if led_on == 6:
-    print('LED ON')
-    LED.LED.value(1)
-  if led_off == 6:
-    print('LED OFF')
-    LED.LED.value(0)
-  response = web_page()
-  conn.send('HTTP/1.1 200 OK\n')
-  conn.send('Content-Type: text/html\n')
-  conn.send('Connection: close\n\n')
-  conn.sendall(response)
-  conn.close()
+    conn, addr = s.accept()
+    print('Got a connection from %s' % str(addr))
+    request = conn.recv(1024)
+    request = str(request)
+    print('Content = %s' % request)
+    led_toggle = request.find('/?led=toggle')
+    temperature_route = request.find('/temperature')
+    if temperature_route == 6:
+        response = temperature_page()
+    elif led_toggle == 6:
+        LED.LED.value(abs(LED.LED.value()-1))
+        response = web_page()
+    else:
+        response = web_page()
+    print('Sending response')
+    conn.send('HTTP/1.1 200 OK\n')
+    conn.send('Content-Type: text/html\n')
+    conn.send('Connection: close\n\n')
+    conn.sendall(response)
+    conn.close()
