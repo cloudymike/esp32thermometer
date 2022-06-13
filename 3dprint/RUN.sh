d7a73b3a08c149b4d3b3ed68c1ec927351b4bcc3
cloudymike <mikael@cloudyforecast.com>
2022-06-12 21:26:01 -0700

    New reference

--- 3dprint/RUN.sh
+++ 3dprint/RUN.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-openscad -o baseplate.stl baseplate.scad | tee baseplate.tmp
+openscad -o baseplate.stl baseplate.scad |& tee baseplate.tmp
 diff baseplate.ref baseplate.tmp
 
-openscad -o topbox.stl topbox.scad | tee topbox.tmp
-diff topbox.ref topbox.tmp
+openscad -o topbox.stl topbox.scad |& tee topbox.tmp
+diff topbox.ref topbox.tmp 
 
- 
+echo Rendering OK
